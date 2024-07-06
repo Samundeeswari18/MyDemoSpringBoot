34bcde754e23a68ac2817d3b247ccb765542cb2d
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-07-06 01:14:10 -0400

    My demo Spring boot application

---
+++ src/main/java/com/sam/mydemospringboot/MyDemoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.sam.mydemospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDemoSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MyDemoSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/mydemospringboot/MyDemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.mydemospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
